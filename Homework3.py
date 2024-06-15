8d128bd5a0457550337c7af9588d52bb883875be
Danil Terentev <danil.terentyev@gmail.com>
2024-06-15 19:11:00 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,5 @@
+num_homeworks = 12
+num_hours = 1.5
+course_name = 'Python'
+average_time = num_hours / num_homeworks
+print('Курс: '+ course_name, 'всего задач:' + str(num_homeworks), 'затрачено часов: '+ str(num_hours), 'среднее время выполнения ' + str(average_time), sep = ', ', end = '.')
